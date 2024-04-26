ee49332adf4552c48bde95b6872ecdfea76d890d
Malik Khubiev <malik.hubiev@mail.ru>
2024-04-26 17:18:56 +0300

    added test and test env

---
+++ client/src/components/ui/CustomAlert/CustomAlert.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { CustomAlert } from './CustomAlert';
+
+describe('CustomAlert', () => {
+  it('должен рендерить сообщение и устанавливать правильный стиль', () => {
+    const message = 'Тестовое сообщение';
+    render(<CustomAlert message={message} />);
+    // Проверка наличия текста сообщения
+    // @ts-ignore
+    expect(screen.getByText(message)).toBeInTheDocument();
+    
+    // Проверка установленного стиля
+    const alertElement = screen.getByRole('alert');
+    // @ts-ignore
+    expect(alertElement).toHaveStyle('position: absolute'); // Проверьте, что у вас правильные стили здесь
+  });
+});
