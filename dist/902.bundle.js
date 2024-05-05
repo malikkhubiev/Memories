12a7f16b1afffa14ab8258b805cb0d7dad726e3d
Malik Khubiev <malik.hubiev@mail.ru>
2024-05-05 18:49:38 +0300

    mb last try

---
+++ dist/902.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[902],{7902:e=>{e.exports=JSON.parse('{"forgotPasHeader":"Забыли пароль","newPasswordLabel":"Новый пароль","confirmPasswordLabel":"Подтверждение пароля","button":"Изменить пароль"}')}}]);
